96243016e0e3371a19926733c55afebb52e79d55
Michael Ferrell <michael.d.ferrell@gmail.com>
2012-08-23 14:42:40 -0500

    adding project files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Boxes::Application.config.secret_token = 'deacacfcc28ade931d8facf1ccf59df9beb7b1631231e547f6c7efe6ef5fd8198d1af089e34d180e45be5e4497b51f5d222a22eda92c5bda89fff74d15755e85'
